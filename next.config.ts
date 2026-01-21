8a0af28f654cf783ee440269eca4aa7ae35cc115
Blake Darling <blakerdarling@gmail.com>
2026-01-20 23:01:09 -0500

    Allow Facebook CDN images in CSP for Spotify profile photos

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
               "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://sdk.scdn.co",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
-              "img-src 'self' data: https://*.scdn.co https://*.spotifycdn.com https://i.scdn.co",
+              "img-src 'self' data: https://*.scdn.co https://*.spotifycdn.com https://i.scdn.co https://*.fbcdn.net",
               "media-src 'self' https://*.scdn.co https://*.spotifycdn.com https://audio-ak.scdn.co",
               "connect-src 'self' https://api.spotify.com https://accounts.spotify.com https://*.supabase.co wss://*.supabase.co https://*.scdn.co",
               "frame-src 'self' https://sdk.scdn.co",
